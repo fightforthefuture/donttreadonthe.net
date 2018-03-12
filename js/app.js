6345b30f70409e255e7d1661639502e24433924e
Ken Mickles <ken@37i.net>
2018-03-12 18:00:03 -0400

    Apply new design

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 			var params = $el.serialize();
 			var $error = $(".form-error");
 			var $success = $(".form-success");
-			var $button = $el.find('button')
+			var $button = $el.find('button');
 			
 			$error.hide();
 			$success.hide();
@@ -30,7 +30,15 @@
 		});
 	}
 
+	function fixFormPosition() {
+		var $copy = $('.section-head .copy');
+		var leftPosition = $copy.offset().left + $copy.width() + 20;
+		$('.form-container').css('left', leftPosition);
+	}
+
 	$(function() {
 		bindForm($("#fftf-form"));
+		fixFormPosition();
+		$(window).resize(fixFormPosition);
 	});
 }(jQuery));
