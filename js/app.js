f29fe2df72f75d73dd9ae162646194b9c32d67c1
Ken Mickles <ken@37i.net>
2018-03-13 15:56:30 -0400

    Add after-action state to form

--- js/app.js
+++ js/app.js
@@ -1,44 +1,75 @@
 (function($){
 	"use strict";
 
-	function bindForm($el, onSuccess) {
-		$el.submit(function(evt){
-			evt.preventDefault();
-			
-			var params = $el.serialize();
-			var $error = $(".form-error");
-			var $success = $(".form-success");
-			var $button = $el.find('button');
-			
-			$error.hide();
-			$success.hide();
-			$("input", $el).attr("disabled", "disabled");
-
-			$.post($el.attr('action'), params, function(response) {
-				if (response.data) {
-					if (onSuccess) {
-						onSuccess();
-					} else {
-						$success.show();
-					}
-				}
-				else {
-					$error.show();
-				}
-				$("input", $el).removeAttr('disabled');
-			});
-		});
-	}
-
 	function fixFormPosition() {
 		var $copy = $('.section-head .copy');
 		var leftPosition = $copy.offset().left + $copy.width() + 20;
 		$('.form-container').css('left', leftPosition);
 	}
 
+	function openPopup(url, title, w, h) {
+		if (!title) {
+			title = 'popup';
+		}
+		if (!w) {
+			w = 600;
+		}
+		if (!h) {
+			h = 500;
+		}
+		// Fixes dual-screen position
+		var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+		var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+		var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+		var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+		var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+		var top = ((height / 2) - (h / 2)) + dualScreenTop;
+		var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+		// Puts focus on the newWindow
+		if (window.focus) {
+			newWindow.focus();
+		}
+	}
+
 	$(function() {
-		bindForm($("#fftf-form"));
 		fixFormPosition();
 		$(window).resize(fixFormPosition);
+
+		$('#fftf-form').submit(function(e){
+			e.preventDefault();
+
+			var $form = $(this);
+			var $error = $form.find('.form-error');
+			var $button = $form.find('.btn');
+			var params = $form.serialize();
+
+			var buttonText = $button.text();
+			$button.text('Loading...').attr('disabled', 'disabled');
+			$error.hide();
+
+			$.post($form.attr('action'), params, function(response){
+				$button.text(buttonText).removeAttr('disabled');
+
+				if (response.data) {
+					$('.form-active, .form-complete').toggleClass('hidden');
+				}
+				else {
+					$error.show();
+				}
+			});
+		});
+
+		$('.btn-facebook').click(function(e){
+			e.preventDefault();
+			openPopup('http://shpg.org/103/186643/facebook', 'facebook');
+		});
+
+		$('.btn-twitter').click(function(e){
+			e.preventDefault();
+			openPopup('http://shpg.org/103/186645/twitter', 'twitter');
+		});
 	});
 }(jQuery));
